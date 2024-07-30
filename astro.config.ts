87cca7b63d5e454a95b2466031eeab84eb2c267c
zhuchentong <2037630@163.com>
2024-07-30 16:29:05 +0800

    update

--- astro.config.ts
+++ astro.config.ts
@@ -1,24 +1,21 @@
-import { defineConfig } from 'astro/config'
-import node from '@astrojs/node'
-import UnoCSS from 'unocss/astro'
-import Vue from '@astrojs/vue'
+import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
+import UnoCSS from 'unocss/astro';
+import Vue from '@astrojs/vue';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: node({
-    mode: 'standalone',
-  }),
-  integrations: [
-    Vue({
-      jsx: true,
-      appEntrypoint: '/src/pages/_app',
-    }),
-    UnoCSS({
-      injectReset: true,
-    }),
-  ],
+  adapter: vercel(),
+  integrations: [Vue({
+    jsx: true,
+    appEntrypoint: '/src/pages/_app'
+  }), UnoCSS({
+    injectReset: true
+  })],
   devToolbar: {
-    enabled: false,
-  },
-})
+    enabled: false
+  }
+});
